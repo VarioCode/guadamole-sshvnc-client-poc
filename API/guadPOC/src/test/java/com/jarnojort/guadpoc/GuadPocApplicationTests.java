231dd89cd74856cdba7d1063d48345c063b08664
Vario <84608017+VarioCode@users.noreply.github.com>
2023-05-09 09:57:30 +0200

    Init commit

---
+++ API/guadPOC/src/test/java/com/jarnojort/guadpoc/GuadPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jarnojort.guadpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuadPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
