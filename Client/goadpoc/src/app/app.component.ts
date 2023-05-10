edd1e7a2ba506d238df8fa927b44d34da445446b
Jort Driegen <jort.driegen@eijsink.nl>
2023-05-10 11:08:21 +0200

    Added js library

--- Client/goadpoc/src/app/app.component.ts
+++ Client/goadpoc/src/app/app.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+
+//@ts-ignore
+import { default as Guacamole } from 'guacamole-common-js';
+
 
 
 @Component({
@@ -6,8 +10,35 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'goadpoc';
 
+  ngOnInit() {
+// Get display div from document
+    // Get display div from document
+    var display = document.getElementById("display");
 
+    // Instantiate client, using an HTTP tunnel for communications.
+    var guac = new Guacamole.Client(
+      new Guacamole.HTTPTunnel("tunnel")
+    );
+
+    if (display == null) return;
+
+    // Add client to display div
+    display.appendChild(guac.getDisplay().getElement());
+
+    // Error handler
+    guac.onerror = function(error:any) {
+      alert(error);
+    };
+
+    // Connect
+    guac.connect();
+
+    // Disconnect on close
+    window.onunload = function() {
+      guac.disconnect();
+    }
+  }
 }
